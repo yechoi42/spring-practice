2a23ee8092db476476e62d8c7b0782fcb53b5635
yechoi <yechoi@student.42seoul.kr>
2022-05-24 22:51:55 +0900

    feat: JPA to Spring data JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -9,27 +9,34 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManager;
+import javax.swing.*;
 
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em; 
+    public SpringConfig(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
     }
+
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        // 직접 설정파일로 빈에 등록하면 아래 한줄로 레포지토리 바꿔끼우기 가능
-//        return new MemoryMemberRepository();
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        // 직접 설정파일로 빈에 등록하면 아래 한줄로 레포지토리 바꿔끼우기 가능
+////        return new MemoryMemberRepository();
+//        return new JpaMemberRepository(em);
+//    }
 
 }
