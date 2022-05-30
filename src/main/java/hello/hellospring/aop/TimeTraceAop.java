ff40adc92135c9f0cd18e6369a34ddc2f3100887
yechoi <yechoi@student.42seoul.kr>
2022-05-30 23:12:22 +0900

    chore: add annotation on AOP
    chore: add annotation on AOP

--- src/main/java/hello/hellospring/aop/TimeTraceAop.java
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -5,6 +5,18 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+/*
+AOP(Aspect-oriented Programming)
+관점 지향 프로그래밍
+흩어진 Aspect를 모듈화 할 수 있는 프로그래밍 기법
+핵심적인 관점 + 부과적인 관점으로 나누어 본다
+
+AOP 적용 전 의존관계
+memberController -> memberService
+
+AOP 적용 후 의존 관계
+memberController -> 프록시 memverService -joinPoint.proceed()> 실제 memberService
+ */
 @Component
 @Aspect
 public class TimeTraceAop {
